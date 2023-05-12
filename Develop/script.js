ad483a56aef2c1ec99a9ebf5e2f0855ef53f60eb
Athena <wallisathena@gmail.com>
2023-05-12 10:29:37 -0700

    Added variable questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var pswdLength= prompt("How many characters would you like your password to be?");
+console.log (pswdLength);
+var lowercase= prompt("Do you want to include lowercase letters?");
+console.log(lowercase);
+var uppercase= prompt("Do you want to include uppercase letters?");
+console.log(uppercase);
+var special= prompt("Do you want to include special characters?");
+console.log(special);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
